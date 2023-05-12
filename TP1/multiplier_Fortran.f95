3777cd7969c37b4619c94014d967caca41972b01
jutogashi <jutogashi@hotmail.com>
2023-05-12 23:21:41 +0200

    correcting bugs

--- TP1/multiplier_Fortran.f95
+++ TP1/multiplier_Fortran.f95
@@ -1,15 +1,16 @@
 program matrix_vector_multiply
     implicit none
 
-    integer, parameter :: max_n = 16384 ! maximum size of vector and matrix
-    integer, parameter :: num_ns = 15 ! number of possible values for N
+    integer, parameter :: num_ns = 25 ! number of possible values for N
     ! possible values for N
     integer, parameter :: ns(num_ns) = [1000, 2000, 3000, 4000, 5000, &
                                         6000, 7000, 8000, 9000, 10000, &
-                                        11000, 12000, 13000, 14000, 15000] 
+                                        11000, 12000, 13000, 14000, 15000, &
+                                        16000, 17000, 18000, 19000, 20000, &
+                                        21000, 22000, 23000, 24000, 25000] 
                                         
-    real, dimension(max_n) :: vector, result
-    real, dimension(max_n,max_n) :: matrix
+    real, dimension(:), allocatable :: x, b
+    real, dimension(:,:), allocatable  :: A
 
     integer :: n, i, j, k ! loop variables
     integer :: start_time, end_time ! variables for timing
@@ -17,22 +18,23 @@ program matrix_vector_multiply
     
     do k = 1, num_ns
         n = ns(k)
+        allocate(A(n,n), x(n), b(n))
         ! initialize the vector with random values between 0 and 1
         call random_seed()
-        call random_number(vector(1:n))
+        call random_number(x(1:n))
 
         ! initialize the matrix with random values between -1 and 1
-        call random_number(matrix(1:n,1:n))
-        matrix(1:n,1:n) = 2.0 * matrix(1:n,1:n) - 1.0
+        call random_number(A(1:n,1:n))
+        A(1:n,1:n) = 2.0 * A(1:n,1:n) - 1.0
 
         ! measure the time taken for matrix-vector multiplication
         call system_clock(start_time, rate)
 
         ! multiply the vector and matrix
         do i = 1, n
-            result(i) = 0.0
+            b(i) = 0.0
             do j = 1, n
-                result(i) = result(i) + matrix(i,j) * vector(j)
+                b(i) = b(i) + A(i,j) * x(j)
             end do
         end do
 
@@ -44,14 +46,16 @@ program matrix_vector_multiply
 
         ! multiply the vector and matrix
         do j = 1, n
-            result(j) = 0.0
+            b(j) = 0.0
             do i = 1, n
-                result(j) = result(j) + matrix(i,j) * vector(i)
+                b(j) = b(j) + A(i,j) * x(i)
             end do
         end do
 
         call system_clock(end_time)
         write(*,*) n, " - Elapsed time col-line: ", real(end_time - start_time) / real(rate), "seconds."
+
+        deallocate(A, x, b)
         
     end do
 
--- TP1/plotter.py
+++ TP1/plotter.py
@@ -2,10 +2,15 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 
-N = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000]
+N = [1000, 2000, 3000, 4000, 5000,
+     6000, 7000, 8000, 9000, 10000,
+     11000, 12000, 13000, 14000, 15000,
+     16000, 17000, 18000, 19000, 20000,
+     21000, 22000, 23000, 24000, 25000]
+
 results = []
 
-filename = "results.txt"
+filename = "COC472/TP1/results.txt"
 
 with open(filename, "r") as file:
     lines = file.readlines()
@@ -26,5 +31,5 @@ plt.xlabel('Size N of vector and Matrix')
 plt.ylabel('Time (s)')
 plt.title('Runtime for multiplication A.x = b', weight = 'bold')
 plt.legend()
-plt.savefig("results.png")
+plt.savefig("COC472/TP1/results.png")
 plt.show()
